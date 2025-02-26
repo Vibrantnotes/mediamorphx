75f8c5ec6bcc4c6a91fac51297bd25cbcf2a72b9
Vibrantnotes <143004414+Vibrantnotes@users.noreply.github.com>
2025-02-26 17:16:07 +0530

    Update youtube.js

--- api/youtube.js
+++ api/youtube.js
@@ -1,1 +1,8 @@
 
+function getYoutubeData(link, callback) {
+	$.get(`https://api.vevioz.com/api/button/mp3/iiMrs3vOm_w/get?video=${link}`, async function (data) {
+		if (typeof callback === "function") {
+			callback(data);
+		}
+	});
+}
