c8a8b54619a94474309c366eee7b71568d9bc263
Vibrantnotes <143004414+Vibrantnotes@users.noreply.github.com>
2025-02-26 17:16:18 +0530

    Delete youtube.js

--- youtube.js
+++
@@ -1,7 +0,0 @@
-function getYoutubeData(link, callback) {
-	$.get(`https://api.vevioz.com/api/button/mp3/iiMrs3vOm_w/get?video=${link}`, async function (data) {
-		if (typeof callback === "function") {
-			callback(data);
-		}
-	});
-}
