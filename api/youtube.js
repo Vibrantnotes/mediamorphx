55f2e1cc912dcf72bc31db554a05c476757dd21d
Vibrantnotes <143004414+Vibrantnotes@users.noreply.github.com>
2025-02-26 17:19:08 +0530

    Update youtube.js

--- api/youtube.js
+++ api/youtube.js
@@ -1,8 +1,23 @@
+import axios from 'axios';
 
-function getYoutubeData(link, callback) {
-	$.get(`https://api.vevioz.com/api/button/mp3/iiMrs3vOm_w/get?video=${link}`, async function (data) {
-		if (typeof callback === "function") {
-			callback(data);
-		}
-	});
+export default async function handler(req, res) {
+    const { video } = req.query;
+
+    if (!video) {
+        return res.status(400).json({ error: "No video link provided" });
+    }
+
+    try {
+        const ytInfoUrl = `https://mediamorphx.vercel.app/get?video=${video}`;
+        const response = await axios.get(ytInfoUrl);
+
+        if (response.status === 200) {
+            res.status(200).json(response.data);
+        } else {
+            res.status(500).json({ error: "Failed to fetch video data" });
+        }
+    } catch (error) {
+        console.error("Error fetching video data:", error.message);
+        res.status(500).json({ error: "Error fetching video data" });
+    }
 }
